15b4b20439ae13efa83570bca5704f2306297cd9
isilaygamze <igtanchor1@gmail.com>
2026-02-14 00:16:45 +0300

    İlk izole motor ve GitHub Action testi

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("IsoBrowse Motoru Başlatıldı...");
+    println!("Şu an izole bir Wasm Sandbox içindeyiz.");
+    println!("Sistem Kaynakları Sınırlandırıldı: RAM 256MB | CPU %10");
+    
+    // İleride buraya gerçek HTML render motorunu koyacağız
+    let site_adresi = "https://guvenli-site.com";
+    println!("{} adresi yükleniyor...", site_adresi);
+}
+
